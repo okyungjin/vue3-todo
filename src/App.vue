bc8690c916f39ecbc44fea8cd5220fa7a08e5935
okyungjin <okyungjin@gmail.com>
2022-04-11 15:02:24 +0900

    ref, reactive 사용하여 reactivity 구현

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,54 @@
 <template>
-  <p>{{ greeting(name) }}</p>
-  <p>{{ greet }}</p>
+  <div>
+    <p>{{ name }}</p>
+    <button class="btn btn-primary" @click="updateName">Change Name</button>
+  </div>
+
+  <div>
+    <p>{{ reactiveInfo.job }}</p>
+    <button class="btn btn-primary" @click="updateReactiveInfo">(Reactive) Change Info</button>
+  </div>
+
+  <div>
+    <p>{{ refInfo.job }}</p>
+    <button class="btn btn-primary" @click="updateRefInfo">(Ref) Change Info</button>
+  </div>
 </template>
 
 <script>
+import { ref, reactive } from 'vue';
+
 export default {
   setup() {
-    const name = 'KyungJin Jung';
+    let name = ref('KyungJin Jung');
+
+    const reactiveInfo = reactive({
+      job: 'developer',
+    });
+
+    const refInfo = ref({
+      job: 'developer',
+    });
 
     const greeting = (name) => `Hello, ${name}`;
 
     const greet = greeting(name);
 
+    const updateName = () => name.value = '정경진';
+
+    const updateReactiveInfo = () => reactiveInfo.job = '(Reactive) Frontend Developer';
+
+    const updateRefInfo = () => refInfo.value.job = '(Ref) Frontend Developer';
+
     return {
       name,
+      reactiveInfo,
+      refInfo,
       greeting,
       greet,
+      updateName,
+      updateReactiveInfo,
+      updateRefInfo,
     }
   }
 }
