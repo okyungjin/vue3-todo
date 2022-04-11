4853f28e9642f609a54e0cd36f4bb2d4865f8902
okyungjin <okyungjin@gmail.com>
2022-04-11 15:56:13 +0900

    examples 소스 import 삭제 및 todo input form 구현

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
-
-  <RefAndReactive></RefAndReactive>
-  <TextInput></TextInput>
+  <div class="container">
+    <h2>To-Do List</h2>
+    <form @submit.prevent="onSubmit" class="d-flex"> <!-- modifier directive -->
+      <div class="flex-grow-1 mr-2">
+        <input class="form-control" type="text" v-model="todo" placeholder="Type New Todo">
+      </div>
+      <div>
+        <button class="btn btn-primary" type="submit">Add</button>
+      </div>
+    </form>
+    {{ todos }}
+  </div>
 </template>
 
 <script>
-import TextInput from './examples/TextInput';
-import RefAndReactive from './examples/RefAndReactive'
+import { ref } from 'vue';
 
 export default {
+  setup() {
+    const todo = ref('');
+    const todos = ref([]);
 
-  components: {
-    TextInput,
-    RefAndReactive,
+    const onSubmit = () => {
+      todos.value.push({
+        id: Date.now(),
+        title: todo.value,
+      })
+    }
+
+    return {
+      todo,
+      todos,
+      onSubmit,
+    }
   }
 }
 </script>
