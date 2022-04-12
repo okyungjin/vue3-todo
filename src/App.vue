3c3c63c3e54cadd695eddc3f71161749a56f5739
okyungjin <okyungjin@gmail.com>
2022-04-12 14:06:35 +0900

    check toggle 기능 v-model 제거 및 emit 사용하도록 변경

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
   <div class="container">
-    <h2>To-Do List</h2>
+    <h2 class="mt-2">To-Do List</h2>
     <TodoSimpleForm @add-todo="addTodo"></TodoSimpleForm>
-
-
-    <div v-if="!todos.length" class="mt-2">추가된 Todo가 없습니다.</div>
-    <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input class="form-check-input" type="checkbox" v-model="todo.done" >
-<!--          <label class="form-check-label" :style="todo.done ? todoStyle : {}">{{ todo.title }}</label>-->
-          <label class="form-check-label" :class="{ done: todo.done }">{{ todo.title }}</label>
-        </div>
-        <div>
-          <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
-        </div>
-      </div>
-    </div>
+    <TodoList :todos="todos" @toggle-todo="toggleTodo"></TodoList>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm';
+import TodoList from './components/TodoList';
 
 export default {
   setup() {
@@ -32,19 +19,20 @@ export default {
 
     const addTodo = (todo) => todos.value.push(todo);
 
-    const deleteTodo = (index) => {
-      todos.value.splice(index, 1);
-      console.log('deleteTodo', index)
-    }
+    const deleteTodo = (index) => todos.value.splice(index, 1);
+
+    const toggleTodo = (index) => todos.value[index].done = !todos.value[index].done;
 
     return {
       todos,
       addTodo,
       deleteTodo,
+      toggleTodo,
     }
   },
   components: {
     TodoSimpleForm,
+    TodoList,
   }
 }
 </script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div v-if="!todos.length" class="mt-2">추가된 Todo가 없습니다.</div>
+  <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
+    <div class="card-body p-2 d-flex align-items-center">
+      <div class="form-check flex-grow-1">
+        <input
+                class="form-check-input"
+                type="checkbox"
+                :value="todo.done"
+                @input="toggleTodo(index)" >
+        <label class="form-check-label" :class="{ done: todo.done }">{{ todo.title }}</label>
+      </div>
+      <div>
+        <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  // props: ['todos'],
+  props: {
+    todos: {
+      type: Array,
+      required: true,
+    }
+  },
+  emits: ['toggle-todo'],
+  setup(props, context) {
+    const toggleTodo = (index) => context.emit('toggle-todo', index);
+
+    return {
+      toggleTodo,
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
