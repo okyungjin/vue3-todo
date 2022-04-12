9a3ea6c1ecfe7c695a0a8f743a9e1ca9e1afb766
okyungjin <okyungjin@gmail.com>
2022-04-12 13:38:49 +0900

    입력 form을 TodoSimpleForm으로 컴포넌트 분리

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit"> <!-- modifier directive -->
-      <div class="d-flex">
-        <div class="flex-grow-1 me-2">
-          <input class="form-control" type="text" v-model="todo" placeholder="Type New Todo">
-        </div>
-        <div>
-          <button class="btn btn-primary" type="submit">Add</button>
-        </div>
-      </div>
-      <div class="mt-2" v-show="hasError" style="color: red">This field cannot be empty</div>
-    </form>
+    <TodoSimpleForm @add-todo="addTodo"></TodoSimpleForm>
+
 
     <div v-if="!todos.length" class="mt-2">추가된 Todo가 없습니다.</div>
     <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
@@ -25,56 +16,35 @@
           <button class="btn btn-danger btn-sm" @click="deleteTodo(index)">Delete</button>
         </div>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm';
 
 export default {
   setup() {
-    const todo = ref('');
     const todos = ref([
       { id: Date.now(), title: 'Sample Todo', done: false }
     ]);
 
-    const onSubmit = () => {
-      if (!todo.value) {
-        hasError.value = true;
-        return;
-      }
-
-      todos.value.push({
-        id: Date.now(),
-        title: todo.value,
-        done: false,
-      });
-      hasError.value = false;
-      todo.value = '';
-    }
-
-    const hasError = ref(false);
-
-    const todoStyle = {
-      textDecoration: 'line-through',
-      color: 'grey',
-    }
+    const addTodo = (todo) => todos.value.push(todo);
 
     const deleteTodo = (index) => {
       todos.value.splice(index, 1);
-
+      console.log('deleteTodo', index)
     }
 
     return {
-      todo,
       todos,
-      onSubmit,
-      hasError,
-      todoStyle,
+      addTodo,
       deleteTodo,
     }
+  },
+  components: {
+    TodoSimpleForm,
   }
 }
 </script>
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="onSubmit"> <!-- modifier directive -->
+    <div class="d-flex">
+      <div class="flex-grow-1 me-2">
+        <input class="form-control" type="text" v-model="todo" placeholder="Type New Todo">
+      </div>
+      <div>
+        <button class="btn btn-primary" type="submit">Add</button>
+      </div>
+    </div>
+    <div class="mt-2" v-show="hasError" style="color: red">This field cannot be empty</div>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup(props, context) {
+    const todo = ref('');
+
+    const onSubmit = () => {
+      if (!todo.value) {
+        hasError.value = true;
+        return;
+      }
+
+      context.emit('add-todo', {
+        id: Date.now(),
+        title: todo.value,
+        done: false,
+      });
+      hasError.value = false;
+      todo.value = '';
+    }
+
+    const hasError = ref(false);
+
+    return {
+      todo,
+      onSubmit,
+      hasError,
+    }
+
+  }
+}
+</script>
