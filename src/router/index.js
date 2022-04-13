ff7449a1dd1d8baa988392c85cae8cf7d944a646
okyungjin <okyungjin@gmail.com>
2022-04-13 18:00:06 +0900

    Todo Page router 기본 구성

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../pages/Index.vue';
 import Todos from '../pages/todos/Index.vue';
+import Todo from '../pages/todos/_id.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/todos',
       name: 'Todos',
       component: Todos,
+    },
+    {
+      path: '/todos/:id',
+      name: 'Todo',
+      component: Todo,
     },
   ],
 });
