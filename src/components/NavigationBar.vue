56c9057f0531d388b186a763c5f5326ca192e50c
okyungjin <okyungjin@gmail.com>
2022-04-14 10:52:20 +0900

    Named Route로 변경

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link class="navbar-brand mx-4" to="/">KyungJin</router-link>
+    <router-link class="navbar-brand mx-4" :to="{ name: 'Home'}">KyungJin</router-link>
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <router-link class="nav-link" to="/todos">Todos</router-link>
+        <router-link class="nav-link" :to="{ name: 'Todos'}">Todos</router-link>
       </li>
     </ul>
   </nav>
