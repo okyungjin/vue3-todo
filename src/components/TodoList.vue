0ff0243cf4a84d67891f5905224008ab3281e41a
okyungjin <okyungjin@gmail.com>
2022-04-18 14:04:12 +0900

    TodoList cursor style로 변경하기

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,7 @@
   </div>
   <div v-for="(todo, index) in todos" :key="todo.id" class="card mt-2">
     <div class="card-body p-2 d-flex align-items-center pointer" @click="moveToTodoItem(todo.id)">
-      <div class="form-check flex-grow-1">
+      <div class="flex-grow-1">
         <input
                 class="form-check-input"
                 type="checkbox"
@@ -12,7 +12,7 @@
                 @change="toggleTodo(index, $event)"
                 @click.stop
         >
-        <label class="form-check-label" :class="{ done: todo.done }">{{ todo.title }}</label>
+        <span class="ms-2" :class="{ done: todo.done }">{{ todo.title }}</span>
       </div>
       <div>
         <button class="btn btn-danger btn-sm" @click.stop="deleteTodo(index)">Delete</button>
