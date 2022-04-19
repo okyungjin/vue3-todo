e01b4228c3785476b7774dc8bec7ecd8897fa347
okyungjin <okyungjin@gmail.com>
2022-04-19 10:19:23 +0900

    axios baseURL 설정

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,20 @@
-import axios from 'axios';
+import axios from '../axios';
 
-const baseUrl = 'http://localhost:3000';
-
-const getTodoItem = (todoId) => axios.get(`${baseUrl}/todos/${todoId}`);
+const getTodoItem = (todoId) => axios.get(`/todos/${todoId}`);
 
 const getTodoList = (page, limit, searchText) => {
-  let uri = `${baseUrl}/todos`;
+  let uri = '/todos';
   if (page) uri += `?_page=${page}&_limit=${limit}&title_like=${searchText}&_sort=id&_order=desc`;
   return axios.get(uri);
 };
 
-const addTodoItem = (todo) => axios.post(`${baseUrl}/todos`, todo);
+const addTodoItem = (todo) => axios.post('/todos', todo);
 
-const deleteTodoItem = (todoId) => axios.delete(`${baseUrl}/todos/${todoId}`);
+const deleteTodoItem = (todoId) => axios.delete(`/todos/${todoId}`);
 
-const patchTodoItem = (todoId, value) => axios.patch(`${baseUrl}/todos/${todoId}`, value);
+const patchTodoItem = (todoId, value) => axios.patch(`/todos/${todoId}`, value);
 
-const putTodoItem = (todoId, value) => axios.put(`${baseUrl}/todos/${todoId}`, value);
+const putTodoItem = (todoId, value) => axios.put(`/todos/${todoId}`, value);
 
 export {
   getTodoItem,
