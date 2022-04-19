db14c013a0ace062ef4fa51e1bde8d6d97df8739
okyungjin <okyungjin@gmail.com>
2022-04-19 11:56:41 +0900

    Toast에 transition 누락되어 추가

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@
   <div class="container">
     <router-view />
   </div>
-  <Toast
-    v-if="showToast"
-    :message="toastMessage"
-    :type="toastType"
-  ></Toast>
+  <transition name="slide">
+    <Toast
+      v-if="showToast"
+      :message="toastMessage"
+      :type="toastType"
+    ></Toast>
+  </transition>
 </template>
 
 <script>
@@ -30,5 +32,22 @@ export default {
     Toast,
   },
 };
-
 </script>
+
+<style scoped>
+
+  .slide-enter-active,
+  .slide-leave-active {
+    transition: all 0.5s ease;
+  }
+  .slide-enter-from,
+  .slide-leave-to {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+  .slide-enter-to,
+  .slide-leave-from {
+    opacity: 1;
+    transform: translateY(0px);
+  }
+</style>
