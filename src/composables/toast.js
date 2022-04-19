70de01738e3b04aa518909694ae5df9f3add1a02
okyungjin <okyungjin@gmail.com>
2022-04-19 11:59:55 +0900

    Vuex toast 모듈로 분리

--- src/composables/toast.js
+++ src/composables/toast.js
@@ -5,11 +5,13 @@ import { useStore } from 'vuex';
 export const useToast = () => {
   const { state, dispatch } = useStore();
 
-  const showToast = computed(() => state.showToast);
-  const toastType = computed(() => state.toastType);
-  const toastMessage = computed(() => state.toastMessage);
+  const showToast = computed(() => state.toast.showToast);
+  const toastType = computed(() => state.toast.toastType);
+  const toastMessage = computed(() => state.toast.toastMessage);
 
-  const triggerToast = (message, type = 'success') => dispatch('triggerToast', { message, type });
+  const triggerToast = (message, type = 'success') => dispatch('toast/triggerToast', { message, type });
+
+  // store.getters['toast/toastMessageWithSmile']
 
   return {
     showToast,
