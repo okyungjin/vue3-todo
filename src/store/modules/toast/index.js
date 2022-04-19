e6e0a7cff5764973f181cc9c1f97fd124524615c
okyungjin <okyungjin@gmail.com>
2022-04-19 12:03:22 +0900

    Vuex toast modules 하위 디렉토리로 분리

---
+++ src/store/modules/toast/index.js
@@ -0,0 +1,27 @@
+export default {
+  namespaced: true,
+  state: {
+    showToast: false,
+    toastMessage: '',
+    toastType: 'success',
+  },
+  // [mutations]는 항상 동기적으로 동작해야 한다.
+  mutations: {
+    UPDATE_TOAST_STATUS(state, payload) {
+      state.showToast = payload;
+    },
+    UPDATE_TOAST_MESSAGE(state, payload) {
+      state.toastMessage = payload;
+    },
+    UPDATE_TOAST_TYPE(state, payload) {
+      state.toastType = payload;
+    },
+  },
+  actions: {
+    triggerToast({ commit }, { message, type }) {
+      commit('UPDATE_TOAST_MESSAGE', message);
+      commit('UPDATE_TOAST_TYPE', type);
+      commit('UPDATE_TOAST_STATUS', true);
+    },
+  },
+};
